2291cf2a5e52becddca18cd5939b28770c9b57fd
ShumpeiKano <b1013025@fun.ac.jp>
2016-09-26 16:31:06 +0900

    サイトポリシー、よくあるご質問も適用

--- js/myscript.js
+++ js/myscript.js
@@ -27,6 +27,7 @@ $(function(){
   $(".move-page-top").click(function(){
     $("html, body").animate({scrollTop:0},"slow");
   });
+  //アコーディオンの中身の縦幅を取得して開く長さを指定
   $(".Panel1").on('click',function(){
     $(".Panel1 .menu_text").toggleClass("active").slideToggle(400);
     $(".Panel1").toggleClass("active");
@@ -43,7 +44,18 @@ $(function(){
     $(".Panel4 .menu_text").toggleClass("active").slideToggle(400);
     $(".Panel4").toggleClass("active");
   });
-
+  $(".Panel5").on('click',function(){
+    $(".Panel5 .menu_text").toggleClass("active").slideToggle(400);
+    $(".Panel5").toggleClass("active");
+  });
+    $(".Panel6").on('click',function(){
+    $(".Panel6 .menu_text").toggleClass("active").slideToggle(400);
+    $(".Panel6").toggleClass("active");
+  });
+  $(".Panel7").on('click',function(){
+    $(".Panel7 .menu_text").toggleClass("active").slideToggle(400);
+    $(".Panel7").toggleClass("active");
+  });
   var nowModalSyncer = null;//現在開かれているモーダルコンテンツ
   var modalClassSyncer = "modal-syncer";//モーダルを開くリンクに付けるクラス名
   var current_scrollY;
