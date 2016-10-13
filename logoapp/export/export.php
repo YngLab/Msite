98da1a40beaf4b60524f0a6adf76b948fed6a72d
sKawashima <bysk.333@gmail.com>
2016-10-13 20:17:41 +0900

    DL成功

--- logoapp/export/export.php
+++ logoapp/export/export.php
@@ -23,31 +23,49 @@ body{
 </style>
 
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+<title>書き出し</title>
+<script src="js/libs/jsgif/LZWEncoder.js"></script>
+<script src="js/libs/jsgif/NeuQuant.js"></script>
+<script src="js/libs/jsgif/GIFEncoder.js"></script>
+<script src="js/libs/jsgif/b64.js"></script>
+
+<script src="js/common_data.js"></script>
+<script src="js/dot_data.js"></script>
+<script src="js/dot_js.js"></script>
+
+<script src="js/dot_export.js"></script>
+
 
 <h1>一覧表示</h1>
 <a href="#dot">dot</a> / <a href="#line">line</a>
-
+<form name="exportForm" id="id_form1" action="">
+サイズ（ピクセル）<input name="size" id="exportSize" type="text" value="1080" />
+アニメーションの間隔（ミリ秒）<input name="size" id="exportSpeed" type="text" value="100" />
 <p id="dot">dot</p>
 
 <?
   $data = mysqli_fetch_array($dot);
   while($data != null){
-    echo "<a href=\"test.php?id=".$data['imgData']."\">"."<img src=\"upload/dot/".$data['ID'].".gif\" class=\"logoapp_sample\" id= ".$data['imgData']."\"></a>";
+    echo "<a href=\"#\" onclick=\"exportImage('".$data['imgData']."','".$data['ID']."');\">"."<img src=\"../upload/dot/".$data['ID'].".gif\" class=\"logoapp_sample\"></a>";
     $data = mysqli_fetch_array($dot);
   }
 ?>
-
+<hr>
 <p id="line">line</p>
 
 <?
   $data = mysqli_fetch_array($line);
   while($data != null){
-    echo "<a href=\"test.php?id=".$data['imgData']."\">"."<img src=\"upload/line/" .$data['ID'] .".gif\" class=\"logoapp_sample\" id= ".$data['imgData']."\"></a>";
+    echo "<a href=\"#\" onclick=\"exportImage('".$data['imgData']."','".$data['ID']."');\">"."<img src=\"../upload/line/" .$data['ID'] .".gif\" class=\"logoapp_sample\"></a>";
     $data = mysqli_fetch_array($line);
   }
 ?>
 
-<script>
+</form>
+
+<div id="exportSpace">0</div>
+
+<!--<script>
   $(function(){
     $(".logoapp_sample").on('click',function(){
       $num = $(this).attr("id");
@@ -57,4 +75,4 @@ body{
       //console.log(<?php echo mysqli_fetch_array(mysqli_query($link, "SELECT imgData FROM `line` WHERE `ID` = 1"))['imgData']; ?>); //phpの値をJSに受け渡せた
     });
   });
-</script>
+</script> -->
