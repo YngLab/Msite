0ee584cdf46a07b084bbca3be32a5b844d63316e
sKawashima <bysk.333@gmail.com>
2016-10-28 19:16:34 +0900

    「あと」「日」をJS出力へ

--- js/countdown.js
+++ js/countdown.js
@@ -1,6 +1,6 @@
 function countdown(eventDate) {
   today = new Date().getDate();
   if(eventDate > today){
-    document.write(eventDate - today);
+    document.write("あと" + (eventDate - today) + "日");
   }
 }
