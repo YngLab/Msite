c0aa6fd2b9454ae280bf6206ce975c94a42b0a6a
sKawashima <bysk.333@gmail.com>
2016-10-28 19:41:36 +0900

    当日・終了の処理を追加

--- js/countdown.js
+++ js/countdown.js
@@ -2,5 +2,9 @@ function countdown(eventDate) {
   today = new Date().getDate();
   if(eventDate > today){
     document.write("あと" + (eventDate - today) + "日");
+  }else if(eventDate == today){
+    document.write("本日");
+  }else{
+    document.write("終了");
   }
 }
