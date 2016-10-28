91783240cf4f0b812badbd06a976d7cc7d1bf933
sKawashima <bysk.333@gmail.com>
2016-10-28 20:01:33 +0900

    複数の日付指定に対応
    argumentsマジ神

--- js/countdown.js
+++ js/countdown.js
@@ -1,5 +1,18 @@
-function countdown(eventDate) {
+function countdown() {
   today = new Date().getDate();
+  for (var i = 0; i < arguments.length; i++) {//複数日程指定時
+    if(arguments[i+1] == undefined){//次の日付がなければ
+      eventDate = arguments[i];
+      break;
+    }else{//次の日付があれば
+      if(arguments[i] >= today){//過ぎていないなら
+        eventDate = arguments[i];
+        break;
+      }else{//過ぎているなら
+        continue;
+      }
+    }
+  }
   if(eventDate > today){
     document.write("あと" + (eventDate - today) + "日");
   }else if(eventDate == today){
@@ -8,3 +21,11 @@ function countdown(eventDate) {
     document.write("終了");
   }
 }
+
+var isset = function(data){
+    if(data === "" || data === null || data === undefined){
+        return false;
+    }else{
+        return true;
+    }
+};
