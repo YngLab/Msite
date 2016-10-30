4f4f4ccef273014cebe98d2fdd88661384b3d0ad
sKawashima <bysk.333@gmail.com>
2016-10-30 18:05:19 +0900

    8桁入力による月・年越し計算に対応
     #227 更新

--- js/countdown.js
+++ js/countdown.js
@@ -3,33 +3,40 @@
  * 引数にイベントの日にちを入れる（月越しは未対応）
  */
 function countdown() {
-  today = new Date().getDate();
   for (var i = 0; i < arguments.length; i++) {//複数日程指定時
     if(arguments[i+1] == undefined){//次の日付がなければ
-      eventDate = arguments[i];
+      leftdays = calc_leftDays(arguments[i]);
       break;
     }else{//次の日付があれば
-      if(arguments[i] >= today){//過ぎていないなら
-        eventDate = arguments[i];
+      leftdays = calc_leftDays(arguments[i]);
+      if(leftdays >= 0){//過ぎていないなら
         break;
       }else{//過ぎているなら
         continue;
       }
     }
   }
-  if(eventDate > today){
-    document.write("あと" + (eventDate - today) + "日");
-  }else if(eventDate == today){
+  if(leftdays > 0){
+    document.write("あと" + leftdays + "日");
+  }else if(leftdays == 0){
     document.write("本日");
   }else{
     document.write("終了");
   }
 }
 
-var isset = function(data){
-    if(data === "" || data === null || data === undefined){
-        return false;
-    }else{
-        return true;
-    }
-};
+function apDay(y,m,d) {//Ref: http://javascript.eweb-design.com/0605_ap.html
+  today = new Date();
+  apday = new Date(y,m-1,d);
+  dayms = 24 * 60 * 60 * 1000;
+  leftdays = Math.floor((apday.getTime()-today.getTime())/dayms) + 1;
+  return(leftdays);
+}
+
+function calc_leftDays(eventDate){//イベントの日付を3つに分ける
+  evyear = parseInt(eventDate / 10000);
+  eventDate -= evyear * 10000;
+  evmonth = parseInt(eventDate / 100);
+  evdate = parseInt(eventDate - evmonth * 100);
+  return(apDay(evyear, evmonth, evdate));
+}
