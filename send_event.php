69ed0f719cc43caadc2764a77402c98a05e1bf21
sKawashima <bysk.333@gmail.com>
2016-11-17 21:12:40 +0900

    デバッグを削除

--- send_event.php
+++ send_event.php
@@ -142,16 +142,16 @@ E_mail：
 【自動追加】内容確認後、処理をお願いします。
 HTML;
 
-echo $return;
+// echo $return;
 
-// mb_language("ja");
-// mb_internal_encoding("UTF-8");
+mb_language("ja");
+mb_internal_encoding("UTF-8");
 
-// if(mb_send_mail($_SESSION['email'],"イベントを申し込み頂きありがとうございます。",$return,$add_header)&&mb_send_mail('info@hakodate-miraiproject.jp',"【イベント申込】".$about,$message,$add_header)){
-// header('Location: recieve_join.html');
-// session_destroy();
-// }else{
-//   echo "error";
-//   session_destroy();
-// }
+if(mb_send_mail($_SESSION['email'],"イベントを申し込み頂きありがとうございます。",$return,$add_header)&&mb_send_mail('info@hakodate-miraiproject.jp',"【イベント申込】".$about,$message,$add_header)){
+header('Location: recieve_join.html');
+session_destroy();
+}else{
+  echo "error";
+  session_destroy();
+}
 ?>
