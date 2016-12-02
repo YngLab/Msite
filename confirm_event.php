66519fb1261b01529460c960cb14b601d6f95afe
sKawashima <bysk.333@gmail.com>
2016-12-03 02:45:02 +0900

    19:野菜

--- confirm_event.php
+++ confirm_event.php
@@ -84,6 +84,7 @@ $_SESSION = $_POST;
     }else if($_POST['id'] == 10){echo '"images/event/c-inoue/02.jpg" alt = "「空気を感じよう！」〜大気圧ってスゴい〜"';
     }else if($_POST['id'] == 14){echo '"images/event/sq_event14.jpg" alt = "親子ワークショップ「レンズのしくみを調べよう！」"';
     }else if($_POST['id'] == 18){echo '"images/event/sq_event18.jpg" alt = "キャンパスワークショップ「新聞紙で遊ぼう！～ヒーローに変身～ 」"';
+    }else if($_POST['id'] == 19){echo '"images/event/sq_event19.jpg" alt = "第1回　おとなの学び講座「やさいのふしぎ」"';
     }
     ?>>
     <p class = "FCHcolor eventabout_title">
@@ -98,7 +99,8 @@ $_SESSION = $_POST;
     }else if($_POST['id'] == 8){echo 'おもしろ写真deアニメーション';
     }else if($_POST['id'] == 10){echo '「空気を感じよう！」〜大気圧ってスゴい〜';
     }else if($_POST['id'] == 14){echo '親子ワークショップ「レンズのしくみを調べよう！」';
-    }else if($_POST['id'] == 14){echo 'キャンパスワークショップ「新聞紙で遊ぼう！～ヒーローに変身～ 」';
+    }else if($_POST['id'] == 18){echo 'キャンパスワークショップ「新聞紙で遊ぼう！～ヒーローに変身～ 」';
+    }else if($_POST['id'] == 19){echo '第1回　おとなの学び講座「やさいのふしぎ」';
     }
     ?></p>
     <div class = "event_point">
@@ -118,6 +120,7 @@ $_SESSION = $_POST;
             }else if($_POST['id'] == 10){echo '11月27日(日)<br>10:30-12:30(1回目)14:00-16:00(2回目)';
             }else if($_POST['id'] == 14){echo '12月25日(日)<br>10:30-12:30(1回目)14:00-16:00(2回目)';
             }else if($_POST['id'] == 18){echo '12月3日(土) 13:00-14:30';
+            }else if($_POST['id'] == 19){echo '12月23日(金) 10:30-12:30';
             }
             ?></td>
         </tr>
@@ -136,6 +139,7 @@ $_SESSION = $_POST;
             }else if($_POST['id'] == 10){echo 'はこだてみらい館';
             }else if($_POST['id'] == 14){echo 'はこだてみらい館';
             }else if($_POST['id'] == 18){echo 'はこだてみらい館　シアター';
+            }else if($_POST['id'] == 19){echo 'はこだてみらい館　シアター';
             }
             ?>
           </td>
@@ -166,6 +170,7 @@ $_SESSION = $_POST;
             }else if($_POST['id'] == 10){echo '小学生～中学生 ※保護者同伴必須';
             }else if($_POST['id'] == 14){echo '小学生～中学生 ※保護者同伴必須';
             }else if($_POST['id'] == 18){echo '5歳～小学生 ※親子参加可';
+            }else if($_POST['id'] == 19){echo '中高生～大人まで';
             }
             ?>
           </td>
--- send_event.php
+++ send_event.php
@@ -17,6 +17,7 @@ if($_SESSION['id'] == "0"){ $about = "はこだてみらい館 先行体験会";
 }else if($_SESSION['id'] == "10"){ $about = "「空気を感じよう！」〜大気圧ってスゴい〜";
 }else if($_SESSION['id'] == "14"){ $about = "親子ワークショップ「レンズのしくみを調べよう！」";
 }else if($_SESSION['id'] == "18"){ $about = "キャンパスワークショップ「新聞紙で遊ぼう！～ヒーローに変身～ 」";
+}else if($_SESSION['id'] == "19"){ $about = "第1回　おとなの学び講座「やさいのふしぎ」";
 }
 
 if(isset($_SESSION['old'])){//年齢入力があればメールに入れる
@@ -86,6 +87,9 @@ if($_SESSION['id'] == 0){$date = '
 }else if($_SESSION['id'] == 18){$date = '
 日時：12月3日(土) 13:00-14:30
 ';
+}else if($_SESSION['id'] == 19){$date = '
+日時：12月23日(金) 10:30-12:30
+';
 }
 
 if($_SESSION['id'] == 0){$place = 'はこだてみらい館';
@@ -100,6 +104,7 @@ if($_SESSION['id'] == 0){$place = 'はこだてみらい館';
 }else if($_SESSION['id'] == 10){$place = 'はこだてみらい館';
 }else if($_SESSION['id'] == 14){$place = 'はこだてみらい館';
 }else if($_SESSION['id'] == 18){$place = 'はこだてみらい館';
+}else if($_SESSION['id'] == 19){$place = 'はこだてみらい館';
 }
 
 if($place == 'はこだてみらい館'){
